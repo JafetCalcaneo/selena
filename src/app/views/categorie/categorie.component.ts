bf9bbee39b0b6cab4f7ad505a06bfc5319d69d30
jafet <jafetcalcaneo2@gmail.com>
2025-01-25 18:55:47 -0600

    add: detailed-view & full view

---
+++ src/app/views/categorie/categorie.component.ts
@@ -0,0 +1,107 @@
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+
+@Component({
+    selector: 'app-categorie',
+    standalone: true,
+    templateUrl: './categorie.component.html',
+    styleUrl: './categorie.component.css'
+})
+export class CategorieComponent {
+
+    constructor(private router: Router){}
+
+    nails: any = [
+        {
+            id: 1,
+            src: '../../../assets/img/foto1.jpeg',
+            type: 'Acrilicas',
+            price: 200,
+        },
+        {
+            id: 2,
+            src: '../../../assets/img/foto2.jpeg',
+            type: 'Acrilicas',
+            price: 200,
+        },
+        {
+            id: 3,
+            src: '../../../assets/img/foto3.jpeg',
+            type: 'Acrilicas',
+            price: 200,
+        },
+        {
+            id: 4,
+            src: '../../../assets/img/foto4.jpeg',
+            type: 'Acrilicas',
+            price: 200,
+        },
+        {
+            id: 5,
+            src: '../../../assets/img/foto5.jpeg',
+            type: 'Acrilicas',
+            price: 200,
+        },
+        {
+            id: 6,
+            src: '../../../assets/img/foto6.jpeg',
+            type: 'Acrilicas',
+            price: 200,
+        },
+        {
+            id: 7,
+            src: '../../../assets/img/foto7.jpeg',
+            type: 'Acrilicas',
+            price: 200,
+        },
+        {
+            id: 8,
+            src: '../../../assets/img/foto8.jpeg',
+            type: 'Acrilicas',
+            price: 200,
+        },
+        {
+            id: 9,
+            src: '../../../assets/img/foto9.jpeg',
+            type: 'Acrilicas',
+            price: 200,
+        },
+        {
+            id: 10,
+            src: '../../../assets/img/foto10.jpeg',
+            type: 'Acrilicas',
+            price: 200,
+        },
+        {
+            id: 11,
+            src: '../../../assets/img/foto11.jpeg',
+            type: 'Acrilicas',
+            price: 200,
+        },
+        {
+            id: 12,
+            src: '../../../assets/img/foto12.jpeg',
+            type: 'Acrilicas',
+            price: 200,
+        },
+        {
+            id: 13,
+            src: '../../../assets/img/foto13.jpeg',
+            type: 'Acrilicas',
+            price: 200,
+        },
+        {
+            id: 14,
+            src: '../../../assets/img/foto14.jpeg',
+            type: 'Acrilicas',
+            price: 200,
+        }
+    ]
+
+
+    openDetailedView(productId: any) {
+        this.router.navigate(['/detailed', productId]);
+    }
+
+
+}
--- src/app/views/detailed-view/detailed-view.component.ts
+++ src/app/views/detailed-view/detailed-view.component.ts
@@ -1,11 +1,25 @@
 import { Component } from "@angular/core";
+import { MatChipsModule } from '@angular/material/chips';
+import { ActivatedRoute } from "@angular/router";
+import { NailsService } from "../../services/nails.service";
+
+
 
 @Component({
     selector: 'app-detailed-view',
     standalone: true,
+    imports: [MatChipsModule],
     templateUrl: './detailed-view.component.html',
     styleUrl: './detailed-view.component.css'
 })
 export class DetailedView {
-    
+
+    constructor(private route: ActivatedRoute, private nailService: NailsService) {}
+
+    product: any = {};
+
+    ngOnInit() {
+        const productId = Number(this.route.snapshot.paramMap.get('id'));
+        this.product = this.nailService.getNailById(productId);
+    }
 }
