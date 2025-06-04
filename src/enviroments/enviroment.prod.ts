42e0fa99858e32bf5e6c2e0770326476df53dc63
jafet <jafetcalcaneo2@gmail.com>
2025-06-04 16:12:14 -0600

    modify: rutas prod

--- src/enviroments/enviroment.prod.ts
+++ src/enviroments/enviroment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    _proxyUser: "https://selenaback.onrender.com:3000/user",
+    _proxyUser: "https://selenaback.onrender.com/user",
     _proxyAuth: "https://selenaback.onrender.com/auth",
     _proxyNail: "https://selenaback.onrender.com/nail",
-    _hostUrl: "https://selenaback.onrender.com:3000"
+    _hostUrl: "https://selenaback.onrender.com"
 }
